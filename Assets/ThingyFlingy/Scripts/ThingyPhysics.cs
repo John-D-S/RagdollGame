a31ce4b749d85ed47047619128c4e15cb0f20e16
John <68535837+John-D-S@users.noreply.github.com>
2021-08-23 21:19:19 +1000

    Thingy physics now work

--- Assets/ThingyFlingy/Scripts/ThingyPhysics.cs
+++ Assets/ThingyFlingy/Scripts/ThingyPhysics.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(ConfigurableJoint))]
 public class ThingyPhysics : MonoBehaviour
 {
     [SerializeField] private List<CollisionDetector> collisionDetectors = new List<CollisionDetector>();
@@ -18,21 +18,58 @@ public class ThingyPhysics : MonoBehaviour
             return false;
         }
     }
+
     [SerializeField] private float standingForce = 3f;
-    [SerializeField] private float standingTorque = 1f;
+    [SerializeField] private float standingBalanceSpring = 1f;
+    [SerializeField] private float standingBalanceDamper = 1f;
 
-    private Rigidbody rigidbody;
-
+    private Rigidbody rb;
+    private ConfigurableJoint configurableJoint;
+    
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        configurableJoint = GetComponent<ConfigurableJoint>();
+    }
+
+    private void UpdateJoint( JointDrive _jointDrive, SoftJointLimitSpring _softJointLimitSpring)
+    {
+        configurableJoint.angularXLimitSpring = _softJointLimitSpring;
+        //configurableJoint.xDrive = _jointDrive;
+        //configurableJoint.yDrive = _jointDrive;
+        //configurableJoint.zDrive = _jointDrive;
+    }
+
+    private void UpdateJointSettings(ref JointDrive _jointDrive, ref SoftJointLimitSpring _softJointLimitSpring)
+    {
+        //linear drive stuff
+        //configurableJoint.targetPosition = transform.position + Vector3.up * 100;
+        //configurableJoint.targetVelocity = Vector3.one * standingSpeed;
+        //_jointDrive.positionSpring = standingSpring;
+        //Debug.Log(_jointDrive.positionSpring);
+        //_jointDrive.positionDamper = 1;
+        //_jointDrive.maximumForce = standingForce;
+        //angular balancing stuff
+        _softJointLimitSpring.damper = standingBalanceDamper;
+        _softJointLimitSpring.spring = standingBalanceSpring;
     }
 
     private void FixedUpdate()
     {
+        SoftJointLimitSpring standingBalancer = new SoftJointLimitSpring();
+        JointDrive standingForceDrive = new JointDrive();
+        UpdateJointSettings(ref standingForceDrive, ref standingBalancer);
+        UpdateJoint(standingForceDrive, standingBalancer);
         if(IsOnGround)
         {
-            rigidbody.AddForce(Vector3.up * standingForce);
+            rb.AddForce(Vector3.up * standingForce);
+            configurableJoint.angularXMotion = ConfigurableJointMotion.Limited;
+            configurableJoint.angularZMotion = ConfigurableJointMotion.Limited;
+        }
+        else
+        {
+            configurableJoint.angularXMotion = ConfigurableJointMotion.Free;
+            configurableJoint.angularZMotion = ConfigurableJointMotion.Free;
         }
     }
 }
