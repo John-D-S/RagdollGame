af4784d952fe1391b5302123fe4156d6bc8529ad
John <68535837+John-D-S@users.noreply.github.com>
2021-08-26 09:52:34 +1000

    Improved Player Controls

--- Assets/ThingyFlingy/Scripts/HoverController.cs
+++ Assets/ThingyFlingy/Scripts/HoverController.cs
@@ -9,12 +9,18 @@ public class HoverController : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float rotationTorque = 5;
-    [SerializeField] private float rotationTorqueDrag = 1;
+    // we use the camera pivot's current rotation to rotate the orb
     [SerializeField] private float movementForce = 1;
+    [SerializeField] private float maxVerticalCameraAngle = 85;
+    [SerializeField] private GameObject cameraGameObject;
+    [SerializeField] private Vector3 cameraOffset = Vector3.back * 10;
+    [SerializeField] private Vector3 cameraLookPosition = Vector3.up;
     
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private Vector3 StabilizingTorque
@@ -26,15 +32,31 @@ public class HoverController : MonoBehaviour
         }
     }
 
-    private Vector3 AimingTorque => new Vector3(mouseDelta.y, mouseDelta.x, 0);
-
+    private Vector3 AimingTorque => new Vector3(0, mouseDelta.x * rotationTorque, 0);
+    
     private Vector2 mouseDelta;
-    private Vector3 TargetForce;
+    private Vector3 unnormalizedDirection;
+    private Vector3 TargetForce => unnormalizedDirection.normalized * movementForce;
+    private float currentCameraXrotation = 0;
     private void Update()
     {
         mouseDelta.x = Input.GetAxisRaw("Mouse X");
         mouseDelta.y = Input.GetAxisRaw("Mouse Y");
-        TargetForce = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * movementForce;
+        unnormalizedDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if(cameraGameObject)
+        {
+            currentCameraXrotation += Input.GetAxisRaw("Mouse Y");
+            if(currentCameraXrotation > maxVerticalCameraAngle)
+            {
+                currentCameraXrotation = maxVerticalCameraAngle;
+            }
+            else if(currentCameraXrotation < -maxVerticalCameraAngle)
+            {
+                currentCameraXrotation = -maxVerticalCameraAngle;
+            }
+            cameraGameObject.transform.position = gameObject.transform.position + gameObject.transform.rotation * Quaternion.Euler(-currentCameraXrotation,0, 0) * cameraOffset;
+            cameraGameObject.transform.LookAt(transform.position + cameraLookPosition);
+        }
     }
 
     private void FixedUpdate()
