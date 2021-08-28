5f446aa57062b180549a894b5248f39524d5ee14
John <68535837+John-D-S@users.noreply.github.com>
2021-08-28 21:00:27 +1000

    ThingyForceField Now works properly

--- Assets/ThingyFlingy/Scripts/Physics/Hover.cs
+++ Assets/ThingyFlingy/Scripts/Physics/Hover.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
+using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Hover : MonoBehaviour
 {
     [SerializeField] private float targetHeight = 3;
+    /*[SerializeField] private List<int> ignoredLayers = new List<int>();
+    private int[] AllIgnoredLayers
+    {
+        get
+        {
+            List<int> allIgnoredLayers = ignoredLayers.ToList();
+            allIgnoredLayers.Add(gameObject.layer);
+            return allIgnoredLayers.ToArray();
+        }
+    }*/
     public float TargetHeight => targetHeight;
     [SerializeField] private float verticalDragModifier = 1;
     private Rigidbody rb;
@@ -18,7 +32,7 @@ public class Hover : MonoBehaviour
             if(rb)
             {
                 RaycastHit hit = new RaycastHit();
-                if(Physics.Raycast(transform.position, Vector3.down, out hit,targetHeight * 2f, ~LayerMask.GetMask(gameObject.layer.ToString())))
+                if(Physics.Raycast(transform.position, Vector3.down, out hit,targetHeight * 2f, ~LayerMask.GetMask("IgnoredByHover")))
                 {
                     float height = hit.distance;
                     return Mathf.Abs(Physics.gravity.y) * targetHeight / height - rb.velocity.y * verticalDragModifier;
--- Assets/ThingyFlingy/Scripts/Physics/ThingyForceField.cs
+++ Assets/ThingyFlingy/Scripts/Physics/ThingyForceField.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.XR;
 
 public class ThingyForceField : MonoBehaviour
 {
-    [SerializeField] private float outerFieldRadius;
-    [SerializeField] private float outerFieldAttractionStrength;
-    [SerializeField] private float innerFieldRadius;
-    [SerializeField] private float innerFieldRepulsionStrength;
+    [SerializeField] private float outerFieldRadius = 5;
+    [SerializeField] private float outerFieldAttractionStrength = 1;
+    [SerializeField] private float innerFieldRadius = 2;
+    [SerializeField] private float innerFieldRepulsionStrength = 1;
+    [SerializeField] private float noForceRadius = 0.25f;
 
+    [SerializeField] private float antiEscapeForceMultiplier;
+
+    private Vector3 thisFamePosition;
+    private Vector3 lastFramePosition;
+    private Vector3 thisFrameVelocity;
+    private Vector3 lastFrameVelocity;
+    private Vector3 accelleration;
+    
+    private void UpdateAccelleration()
+    {
+        lastFramePosition = thisFamePosition;
+        thisFamePosition = transform.position;
+        lastFrameVelocity = thisFrameVelocity;
+        thisFrameVelocity = thisFamePosition - lastFramePosition;
+        accelleration = (thisFrameVelocity - lastFrameVelocity) / Time.fixedDeltaTime;
+        //Debug.Log($"thisFrameSpeed = {thisFrameVelocity.magnitude}, accelleration = {accelleration.magnitude}");
+    }
+    
     private Vector3 RepulsionForce(Vector3 _position)
     {
         Vector3 positionRelativeToFieldCenter = gameObject.transform.position - _position;
@@ -32,23 +53,36 @@ public class ThingyForceField : MonoBehaviour
     {
         return -Physics.gravity;
     }
+
+    private Vector3 AntiEscapeForce(Rigidbody _rigidbody)
+    {
+        float dotFromPosToRBPos = Vector3.Dot(_rigidbody.velocity, _rigidbody.position - transform.position); 
+        if(dotFromPosToRBPos > 0)
+        {
+            return -_rigidbody.velocity * antiEscapeForceMultiplier * dotFromPosToRBPos;
+        }
+        return Vector3.zero;
+    }
     
     private void ApplyForceField(ref Rigidbody _rigidbody)
     {
         Vector3 rbPosition = _rigidbody.position;
-        float rbDistanceFromCenterOfForceField;
         Vector3 forceToAdd = new Vector3();
-        if(Vector3.Distance(transform.position, rbPosition) < innerFieldRadius)
+        float distanceToRB = Vector3.Distance(transform.position, rbPosition);
+        if(distanceToRB < noForceRadius) { /*do nothing*/ }
+        else if(distanceToRB < innerFieldRadius)
         {
             forceToAdd += RepulsionForce(rbPosition);
         }
-        else if  (Vector3.Distance(transform.position, rbPosition) < outerFieldRadius)
+        else if(distanceToRB < outerFieldRadius)
         {
             forceToAdd += AttractionForce(rbPosition);
         }
         forceToAdd += AntiGravityForce();
         forceToAdd *= _rigidbody.mass;
+        forceToAdd += AntiEscapeForce(_rigidbody);
         _rigidbody.AddForce(forceToAdd);
+        _rigidbody.velocity += accelleration;
     }
 
     private List<ThingyPhysics> GetThingiesInForceField()
@@ -63,5 +97,32 @@ public class ThingyForceField : MonoBehaviour
             }
         }
         return returnValue;
+    }
+
+    private void Start()
+    {
+        thisFamePosition = lastFramePosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateAccelleration();
+        List<ThingyPhysics> thingysInForceField = GetThingiesInForceField();
+        foreach(ThingyPhysics thingyPhysics in thingysInForceField)
+        {
+            foreach(Rigidbody iteratedRigidbody in thingyPhysics.ConnectedRigidbodies)
+            {
+                Rigidbody thingyRigidbody = iteratedRigidbody;
+                ApplyForceField(ref thingyRigidbody);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, outerFieldRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, innerFieldRadius);
     }
 }
